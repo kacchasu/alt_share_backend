3d448a79c201adf0419b0649c0bd203e992583bc
german <7germania7@gmail.com>
2023-05-24 21:57:08 +0300

    1_1

---
+++ src/main/java/com/queerxdisasster/altshare/AltShareApplication.java
@@ -0,0 +1,10 @@
+package com.queerxdisasster.altshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AltShareApplication.class, args
---
+++ src/test/java/com/queerxdisasster/altshare/AltShareApplicationTests.java
@@ -0,0 +1,10 @@
+package com.queerxdisasster.altshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltShareApplicationTests {
+
+    @Test
+    void contextLoads() {
